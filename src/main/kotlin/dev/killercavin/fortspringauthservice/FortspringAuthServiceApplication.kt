8e33082f437cc28b25194d24be3a8425221474df
Cavin <114779273+Killercavin@users.noreply.github.com>
2025-10-30 10:42:30 +0300

    Remove @EnableJdbcAuditing annotation
    Removed the @EnableJdbcAuditing annotation from the application class.

--- src/main/kotlin/dev/killercavin/fortspringauthservice/FortspringAuthServiceApplication.kt
+++ src/main/kotlin/dev/killercavin/fortspringauthservice/FortspringAuthServiceApplication.kt
@@ -2,9 +2,7 @@ package dev.killercavin.fortspringauthservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 
-@EnableJdbcAuditing
 @SpringBootApplication
 class FortspringAuthServiceApplication
 
